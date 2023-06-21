acbca5d24db06141601302ad69a0c2e2febe5fdd
Nash <nashwanchowder@gmail.com>
2023-06-21 16:25:36 -0400

    newfiles

---
+++ src/main/java/org/example/DatabaseConnector.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.sql.*;
+
+public class DatabaseConnector {
+    public static Connection connect() {
+        Connection connection = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/yourDatabase";
+            String username = "root";
+            String password = "Balo1234";
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Successfully connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    public void selectAllBooks() {
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            connection = this.connect();
+            String sql = "SELECT * FROM books";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("title") + "\t" +
+                        rs.getString("author") + "\t" +
+                        rs.getDouble("price"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error executing SELECT statement");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
